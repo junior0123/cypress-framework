9bf4c75d7133d5bb855e732212b83d1af314c3d0
Alvaro Sivila <alvarogerardojunior@gmail.com>
2024-04-12 16:20:27 -0400

    Create classes for Common page

---
+++ cypress/e2e/pages/common-page/common-page.elements.js
@@ -0,0 +1,25 @@
+export class CommonPageElements {
+    static get topMenu() {
+        return {
+            get Home() {
+                return cy.get('li.active a');
+            },
+            get contact() {
+                return cy.get('a[data-target="#exampleModal"]');
+            },
+            get aboutUs() {
+                return cy.get('a[data-target="#videoModal"]');
+            },
+            get cart() {
+                return cy.contains('a', 'Cart');
+            },
+            get login() {
+                return cy.get('a[data-target="#logInModal"]');
+            },
+            get signup() {
+                cy.get('a[data-target="#signInModal"]');
+            }
+
+        }
+    }
+}
